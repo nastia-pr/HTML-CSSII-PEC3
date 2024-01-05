ef4922311dae950e69d4559fec37a5ce0622d20c
nastia-pr <apronyuk@uoc.edu>
2024-01-05 21:50:49 +0100

    Changes PEC3

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -6,11 +6,22 @@
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
 
+import mapboxgl from 'mapbox-gl';
+
 /**
  * Write any other JavaScript below
  */
 
-+( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-} )();
+mapboxgl.accessToken = "pk.eyJ1IjoibmFzbmFzdGkiLCJhIjoiY2xwczl3d3V3MDFoZTJqcXM5N3NremZmZiJ9.7SlUptC06BFl3rrYk3jYtQ";
+
+const santiagoDeCompostelaCoordinates = [-8.5449, 42.8782];
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+  center: santiagoDeCompostelaCoordinates,
+  zoom: 9
+});
+
+
+new mapboxgl.Marker().setLngLat(santiagoDeCompostelaCoordinates).addTo(map);
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        headerBackground: '#0067aa',
+        contactBackground: "#edfffa80",
+        cardsBackground: "#91d3f0",
+        linkHover: '#9aff5c',
+        readMoreHover: '#0e02b8',
+        footerBackground: '#05498e',
+        contactInput: "#72d3ff40",
+        contactFormText: '#ff3cf6',
+        contactMapText: "#00fbf8"
+      },
+
+      fontFamily: {
+        sans: ['Urbanist', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
